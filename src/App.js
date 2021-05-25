3a9198ed7e1f25a2ad17e636de48897e119e4b14
Aleena MIshra <56436393+Aleena-Mishra-10@users.noreply.github.com>
2021-05-25 23:20:49 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-import "./styles.css";
+import React from "react";
+import SearchBar from "/src/components/SearchBar";
+import youtube from "/src/apis/youtube";
+import VideoList from "/src/components/VideoList";
+import VideoDetail from "/src/components/VideoDetail";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit("buildings");
+  }
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term
+      }
+    });
+
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+export default App;
